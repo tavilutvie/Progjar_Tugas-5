67ed4143e22d9f8b4b22dd487351cfe6b75cb045
Eldenabih Tavirazin Lutvie <tavirazin@gmail.com>
2023-06-17 23:51:19 +0700

    Submit tugas 5 progjar

---
+++ README.md
@@ -0,0 +1,5 @@
+# <b>Pemrograman Jaringan A</b>
+## Tugas 5
+### Nama : Eldenabih Tavirazin Lutvie
+### NRP : 5025201213
+<hr>
---
+++ runserver.sh
@@ -0,0 +1,17 @@
+#jalankan 5 async_server
+
+# Untuk server asyncronus
+# python3 async_server.py 8001 &
+# python3 async_server.py 8002 &
+# python3 async_server.py 8003 &
+# python3 async_server.py 8004 &
+
+
+# Untuk server process pool http
+python3 server_process_pool_http.py 8021 &
+python3 server_process_pool_http.py 8022 &
+python3 server_process_pool_http.py 8023 &
+python3 server_process_pool_http.py 8024 &
+
+
+
